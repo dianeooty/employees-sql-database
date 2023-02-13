30390fff2de4cd96b83b788febdb1ce4124a43b1
Diane Guzman <117790100+dianeooty@users.noreply.github.com>
2023-02-13 15:39:09 -0800

    updates

--- schematas.sql
+++ schematas.sql
@@ -1,30 +1,30 @@
 -- CREATING ALL TABLES
 
 CREATE TABLE dept(
-	dept_no VARCHAR(50) PRIMARY KEY NOT NULL,
-	dept_name VARCHAR(50) NOT NULL
+	dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employees(
 	emp_no INT PRIMARY KEY NOT NULL,
-	emp_title_id VARCHAR(50) NOT NULL,
+	emp_title_id VARCHAR(10) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
-	sex VARCHAR(10) NOT NULL,
+	sex VARCHAR(2) NOT NULL,
 	hire_date DATE NOT NULL
 );
 
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(50) NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES dept(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 	
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(50) NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES dept(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
@@ -37,6 +37,6 @@ CREATE TABLE salaries(
 );
 
 CREATE TABLE titles(
-	title_id VARCHAR(50) PRIMARY KEY NOT NULL,
-	title VARCHAR(50) NOT NULL
+	title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
 );
